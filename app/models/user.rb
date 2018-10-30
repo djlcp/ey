5f2e7e9cc07de04e218cc731d5acfe49ff1ef39f
richard-digitaljersey <RJS@Mac-The-Knife.local>
2018-10-30 19:18:56 +0000

    Email stuff

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+## users model created by Richard 30.10.18 as a test for csv output ##
+## need to test this!! Needs a users data set before this will work ##
+
+class User < ApplicationRecord
+	acts_as_paranoid
+	# Include default devise modules. 
+	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :tracka
+
+	validates :email, presence: true
+
+	## below some active record assocation examples - variables not yet confirmed 
+
+	# has_many :<%DATABASE ITEM GOES HERE
+
+	def self.assign_from_row(row)
+		user = User.where(email: row[:email]).first_or_initialize
+		user.assign_attributes row.to_hash.slice(:first_name, :last_name)
+	end
+
+	## this includes headers
+	## database items need to match the %w{} array! 
+
+	def self.to_csv(options = {}) 
+		attributes = %w{id email first_name last_name}
+
+		# HOW TO GET BOTH 'OPTIONS' and 'HEADERS?'
+		CSV.generate(options, headers: true) do |csv|
+			csv << attributes
+
+			User.all.each do |user|
+				attributes.each do |att|
+					csv << user.send(att)
+				end
+			end
+		end
+	end
+end
