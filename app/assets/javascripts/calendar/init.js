46553c15f9169df3a3a774858c62ec7f10a0be64
Bill Rogers <billyrogers902003@yahoo.co.uk>
2018-10-30 19:07:41 +0000

    Added example ajax call

--- app/assets/javascripts/calendar/init.js
+++ app/assets/javascripts/calendar/init.js
@@ -1,4 +1,12 @@
 $(document).on('turbolinks:load', function() {
+  // $.ajax({
+  //   url: '/users',
+  //   type: 'GET',
+  //   dataType: 'json',
+  //   success: function(response) {
+  //     console.log(response)
+  //   }
+  // });
   $('#calendar').fullCalendar({
     dayClick: function(date, jsEvent, view) {
       alert('a day has been clicked!');
