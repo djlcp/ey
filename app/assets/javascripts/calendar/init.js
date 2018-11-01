15a79e6889c78dab35ccc0018c9b0258d78521b1
Elita Judjallo <elita.judjallo@gmail.com>
2018-11-01 18:08:17 +0000

    custamize fullcalendar

--- app/assets/javascripts/calendar/init.js
+++ app/assets/javascripts/calendar/init.js
@@ -8,8 +8,27 @@ $(document).on('turbolinks:load', function() {
   //   }
   // });
   $('#calendar').fullCalendar({
-    dayClick: function(date, jsEvent, view) {
-      alert('a day has been clicked!');
-    }
-  })
-});
+    /*dayClick: function(date, jsEvent, view) {
+      alert('a day has been clicked!');*/
+  themeSystem: 'jquery-ui',
+      header: {
+        left: 'month,agendaWeek,agendaDay,listMonth',
+        center: 'title',
+        right: 'today, prevYear,prev,next,nextYear'
+      },
+      //CHANGE BUTTON TEXT FOR PREV,NEXT YEAR
+      buttonText: {
+         prevYear: new moment().year() - 1,
+         nextYear: new moment().year() + 1
+      },
+      /*viewRender: function (view) {
+        var y = moment($('#calendar').fullCalendar('getDate')).year();
+        $(".fc-prevYear-button").text(y - 1);
+        $(".fc-nextYear-button").text(y + 1);
+      }*/
+
+      weekNumbers: true,
+      eventLimit: true, // allow "more" link when too many events
+      themeSystem: "jquery-ui"
+    });
+ });
