39ed7c6bb49af664ad60b1df414483a22a546d35
Elita Judjallo <elita.judjallo@gmail.com>
2018-11-13 19:42:18 +0000

    updated calendar

--- app/assets/javascripts/calendar/init.js
+++ app/assets/javascripts/calendar/init.js
@@ -10,7 +10,12 @@ $(document).on('turbolinks:load', function() {
   $('#calendar').fullCalendar({
     /*dayClick: function(date, jsEvent, view) {
       alert('a day has been clicked!');*/
-  themeSystem: 'jquery-ui',
+      themeSystem: 'jquery-ui',
+      firstDay: 1, // week starts from Monday
+      fixedWeekCount: false, //caledar will have 4,5 or 6 weeks,depending on the month
+      contentHeight: 600, // display height
+      Boolean, default: true, //?!! auto-resize for browser window
+
       header: {
         left: 'month,agendaWeek,agendaDay,listMonth',
         center: 'title',
@@ -21,14 +26,20 @@ $(document).on('turbolinks:load', function() {
          prevYear: new moment().year() - 1,
          nextYear: new moment().year() + 1
       },
+      minTime: "07:00:00",
+      maxTime: "19:00:00", // shows time for weeks and days
       /*viewRender: function (view) {
         var y = moment($('#calendar').fullCalendar('getDate')).year();
         $(".fc-prevYear-button").text(y - 1);
         $(".fc-nextYear-button").text(y + 1);
       }*/
 
-      weekNumbers: true,
+      weekNumbers: true, // shows week count
       eventLimit: true, // allow "more" link when too many events
+      selectable: true, //will give ability to select multiple days
+      selectHelper: true,
+      editable: true,
+
       themeSystem: "jquery-ui"
     });
  });
