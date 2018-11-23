3de5408adb4406c3f024345daf5d1a2e7c3f9627
Keishma <kieran.l@yandex.com>
2018-11-23 20:02:47 +0000

    add extra form fields

--- app/controllers/hr/users_controller.rb
+++ app/controllers/hr/users_controller.rb
@@ -33,7 +33,7 @@ class Hr::UsersController < ApplicationController
 	end
 
 	def user_params
-		params.require(:user).permit(:first_name)
+		params.require(:user).permit(:first_name, :last_name, :email, :role)
 	end
 
 end
