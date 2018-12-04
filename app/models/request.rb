da6ab639e1657645c4bc8e57622e50bcf35a689d
Matt Chatterley <matt@code.je>
2018-12-04 19:54:31 +0000

    Tidy form

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,6 @@
 class Request < ApplicationRecord
+	LEAVE_TYPES = [['Holiday', 1], ['Illness', 2], ['Study Leave', 3], ['Maternity', 4], ['Paternity', 5], ['Unpaid', 6], ['Other', 7]]
+
 	enum leave_type: {
 		holiday: 1,
 		illness: 2,
@@ -8,6 +10,8 @@ class Request < ApplicationRecord
     unpaid: 6,
     other: 7
 	}
+
+
 
 	enum approval: [:pending, :approved, :declined]
 
