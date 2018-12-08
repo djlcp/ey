aebe8d7f3d066387557978c184f7c5f2cb171c4f
Bill Rogers <billyrogers902003@yahoo.co.uk>
2018-12-08 17:11:54 +0000

    Added filterable table to the manager show page

--- app/controllers/manager/users_controller.rb
+++ app/controllers/manager/users_controller.rb
@@ -1,8 +1,16 @@
 class Manager::UsersController < ApplicationController
   before_action :authenticate_user!, :is_a_manager?
+  before_action :set_start_date, only: [:index]
+  before_action :set_end_date, only: [:index]
+  before_action :set_approval_status, only: [:index]
 
   def index
     @users = current_user.managed_users
+    requests = current_user.managed_user_requests
+    requests = requests.where('start >= :date', date: @start_date) if @start_date
+    requests = requests.where('end <= :date', date: @end_date) if @end_date
+    requests = requests.send(@approval_status) if @approval_status
+    @requests = requests
   end
 
   def show
@@ -15,5 +23,32 @@ class Manager::UsersController < ApplicationController
     unless current_user.admin? || current_user.is_manager?
       redirect_to root_url, alert: 'Access Denied'
     end
+  end
+
+  def set_start_date
+    @start_date =
+      if params[:start_date].present?
+        Date.parse(params[:start_date])
+      else
+        nil
+      end
+  end
+
+  def set_end_date
+    @end_date =
+      if params[:end_date].present?
+        Date.parse(params[:end_date])
+      else
+        nil
+      end
+  end
+
+  def set_approval_status
+    @approval_status =
+      if params[:approval_status].present? && params[:approval_status] != 'all'
+        params[:approval_status]
+      else
+        nil
+      end
   end
 end
